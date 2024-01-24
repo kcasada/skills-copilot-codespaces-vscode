831512fea71cf3c23a97a77500430342f56e5268
kcasada <13481371+kcasada@users.noreply.github.com>
2024-01-24 07:47:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsTitle = document.getElementById("memberSkillsTitle");
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsListItems = document.getElementsByClassName("memberSkillsListItem");
+    var memberSkillsListItemsLength = memberSkillsListItems.length;
+    var memberSkillsListItemsWidth = memberSkillsListItems[0].offsetWidth;
+    var memberSkillsListItemsWidthTotal = memberSkillsListItemsWidth * memberSkillsListItemsLength;
+    var memberSkillsListItemsWidthTotalString = memberSkillsListItemsWidthTotal.toString();
+    var memberSkillsListItemsWidthTotalStringPx = memberSkillsListItemsWidthTotalString + "px";
+    var memberSkillsListItemsWidthTotalStringPxNegative = "-" + memberSkillsListItemsWidthTotalStringPx;
+    var memberSkillsListItemsWidthTotalStringPxNegativeInt = parseInt(memberSkillsListItemsWidthTotalStringPxNegative);
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntString = memberSkillsListItemsWidthTotalStringPxNegativeInt.toString();
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPx = memberSkillsListItemsWidthTotalStringPxNegativeIntString + "px";
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxInt = parseInt(memberSkillsListItemsWidthTotalStringPxNegativeIntStringPx);
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalf = memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxInt / 2;
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfString = memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalf.toString();
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfStringPx = memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfString + "px";
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfStringPxInt = parseInt(memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfStringPx);
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfStringPxIntNegative = "-" + memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfStringPxInt;
+    var memberSkillsListItemsWidthTotalStringPxNegativeIntStringPxIntHalfStringPxIntNegativeInt = parseInt(memberSkillsList
